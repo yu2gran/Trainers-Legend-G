24805849117e19abfb8fc7b16688501357d85b7d
yu2gran <118958555+yu2gran@users.noreply.github.com>
2022-11-29 11:02:50 +0900

    Update camera.cpp

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -25,8 +25,8 @@ namespace UmaCamera {
 		float moveStep = 0.1;
 		float look_radius = 9.0;  // 转向半径
 		float moveAngel = 3.5;  // 转向角度
-		float NearClipPlane = 0.1,
-                float FarClipPlane = 200.0,
+		float NearClipPlane = 0.1;
+                float FarClipPlane = 200.0;
 		float horizontalAngle = 0;  // 水平方向角度
 		float verticalAngle = 0;  // 垂直方向角度
 
